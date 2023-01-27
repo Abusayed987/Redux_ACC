c9015cff7e2d51ef8fe4d923fbb38476e8717046
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2023-01-27 17:57:03 +0600

    project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Parent from './Pages/Parent';
+
+function App() {
+  return (
+    <div>
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Child.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Child = () => {
+    return (
+        <div>
+            {/* Theme setup completed now  Don't change after 1 months  */}
+        </div>
+    );
+};
+
+export default Child;
